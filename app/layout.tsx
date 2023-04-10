4ef0b64a537b06bb24f23777f23dc64844fec970
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-04-10 21:04:39 +0500

    Integrate provider in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import Providers from './components/Providers'
+import Navbar from './components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({
@@ -8,7 +13,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+<body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+        <Providers>
+        {/* @ts-expect-error Server Component */}
+
+          <Navbar />
+          {/* <Toaster position='bottom-right' />
+
+          <MobileMenu /> */}
+
+          <main>{children}</main>
+        </Providers>
+
+        {/* Allow more height for mobile menu on mobile */}
+        <div className='h-40 md:hidden' />
+      </body>
     </html>
   )
 }
